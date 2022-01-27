948048332758ab41d8622a09136b48af771b1356
WilkerB-12 <wilkerblanco18@gmail.com>
2022-01-27 23:42:32 +0000

    primera entrega

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -6,19 +6,31 @@ import rigoImage from "../../img/rigo-baby.jpg";
 //create your first component
 const Home = () => {
 	return (
-		<div>
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
+		<div className="container-fluid text-center bground vh-100">
+			<h1 className="pt-3 fw-lighter">todos</h1>
+			<div className="abs-center text-start">
+				<ul className="list-group list-group-flush w-75">
+					<li className="list-group-item border">
+						<input
+							className="w-100"
+							type="text"
+							placeholder="what needs to be done?"
+						/>
+					</li>
+					<li className="list-group-item border border-top-0">
+						A second item
+					</li>
+					<li className="list-group-item border border-top-0">
+						A third item
+					</li>
+					<li className="list-group-item border border-top-0">
+						A fourth item
+					</li>
+					<li className="list-group-item border border-top-0">
+						And a fifth one
+					</li>
+				</ul>
+			</div>
 		</div>
 	);
 };
