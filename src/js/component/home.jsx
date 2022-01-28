7f634a0a7fa991bedb20c8596a36100bcde1fc9c
WilkerB-12 <wilkerblanco18@gmail.com>
2022-01-28 22:50:40 +0000

    segundo commit

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,10 +1,11 @@
-import React from "react";
+import React, { useState } from "react";
 
 //include images into your bundle
 import rigoImage from "../../img/rigo-baby.jpg";
 
 //create your first component
 const Home = () => {
+	const [Task, setTask] = useState([]);
 	return (
 		<div className="container-fluid text-center bground vh-100">
 			<h1 className="pt-3 fw-lighter">todos</h1>
@@ -15,19 +16,13 @@ const Home = () => {
 							className="w-100"
 							type="text"
 							placeholder="what needs to be done?"
+							value={Task}
+							onKeyPress={(e) => {
+								e.key === "Enter"
+									? setTask(e.target.value)
+									: console.log("pulsa enter");
+							}}
 						/>
-					</li>
-					<li className="list-group-item border border-top-0">
-						A second item
-					</li>
-					<li className="list-group-item border border-top-0">
-						A third item
-					</li>
-					<li className="list-group-item border border-top-0">
-						A fourth item
-					</li>
-					<li className="list-group-item border border-top-0">
-						And a fifth one
 					</li>
 				</ul>
 			</div>
