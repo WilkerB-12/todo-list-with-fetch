0a4d28a77b1a1e160e9e60accf4bf953c1744d25
WilkerB-12 <wilkerblanco18@gmail.com>
2022-01-29 17:49:49 +0000

    input listo

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,3 +1,4 @@
+import { arrayOf } from "prop-types";
 import React, { useState } from "react";
 
 //include images into your bundle
@@ -5,7 +6,8 @@ import rigoImage from "../../img/rigo-baby.jpg";
 
 //create your first component
 const Home = () => {
-	const [Task, setTask] = useState([]);
+	const [tasklist, setTasklist] = useState([]);
+	const [task, setTask] = useState("");
 	return (
 		<div className="container-fluid text-center bground vh-100">
 			<h1 className="pt-3 fw-lighter">todos</h1>
@@ -16,14 +18,31 @@ const Home = () => {
 							className="w-100"
 							type="text"
 							placeholder="what needs to be done?"
-							value={Task}
+							value={task}
+							onChange={(evento) => {
+								setTask(evento.target.value);
+							}}
 							onKeyPress={(e) => {
-								e.key === "Enter"
-									? setTask(e.target.value)
-									: console.log("pulsa enter");
+								let newTasklist = tasklist.map((newtask, i) => {
+									return newtask;
+								});
+								newTasklist.push(task);
+								if (e.key === "Enter") {
+									setTasklist(newTasklist);
+									setTask("");
+								}
 							}}
 						/>
 					</li>
+					{tasklist.map((tarea, i) => {
+						return (
+							<li
+								className="list-group-item border border-top-0"
+								key={i}>
+								{tarea}
+							</li>
+						);
+					})}
 				</ul>
 			</div>
 		</div>
