9c1fed455421311ab8cc95b9972a2ffef1ec737a
WilkerB-12 <wilkerblanco18@gmail.com>
2022-02-02 14:55:41 +0000

    primer commit: llenado de lista y Api completo

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,5 +1,5 @@
 import { arrayOf } from "prop-types";
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 
 //include images into your bundle
 import rigoImage from "../../img/rigo-baby.jpg";
@@ -7,7 +7,46 @@ import rigoImage from "../../img/rigo-baby.jpg";
 //create your first component
 const Home = () => {
 	const [tasklist, setTasklist] = useState([]);
-	const [task, setTask] = useState("");
+	const [task, setTask] = useState({
+		label: "",
+		done: "",
+	});
+	const getToDos = async () => {
+		const response = await fetch(
+			"https://assets.breatheco.de/apis/fake/todos/user/wblanco12"
+		);
+		const dataAsJSON = await response.json();
+		if (!response.ok) {
+			createUserList();
+			console.log(dataAsJSON);
+			return;
+		}
+		setTasklist(dataAsJSON);
+		console.log(dataAsJSON);
+	};
+	const createUserList = async () => {
+		const response = await fetch(
+			"https://assets.breatheco.de/apis/fake/todos/user/wblanco12",
+			{
+				method: "Post",
+				headers: { "Content-Type": "application/json" },
+				body: JSON.stringify([]),
+			}
+		);
+	};
+	const updateTaskList = async (newTasklist) => {
+		const response = await fetch(
+			"https://assets.breatheco.de/apis/fake/todos/user/wblanco12",
+			{
+				method: "Put",
+				headers: { "Content-Type": "application/json" },
+				body: JSON.stringify(newTasklist),
+			}
+		);
+	};
+	useEffect(() => {
+		getToDos();
+	}, []);
 	return (
 		<div className="container-fluid text-center bground vh-100">
 			<h1 className="pt-3 fw-lighter">todos</h1>
@@ -18,25 +57,38 @@ const Home = () => {
 							className="w-100"
 							type="text"
 							placeholder="what needs to be done?"
-							value={task}
+							value={task.label}
 							onChange={(evento) => {
-								setTask(evento.target.value);
-							}}
-							onKeyPress={(e) => {
-								let newTasklist = tasklist.map((newtask, i) => {
-									return newtask;
+								setTask({
+									label: evento.target.value,
+									done: false,
 								});
+							}}
+							onKeyPress={async (e) => {
+								const newTasklist = tasklist.map(
+									(newtask, i) => {
+										return newtask;
+									}
+								);
 								newTasklist.push(task);
 								if (e.key === "Enter") {
-									setTasklist(newTasklist);
-									setTask("");
+									setTask({
+										label: "",
+										done: "",
+									});
+									await updateTaskList(newTasklist);
+									getToDos();
 								}
 							}}
 						/>
 					</li>
 					{tasklist.map((tarea, i) => {
 						return (
-							<li className="list-group-item border border-top-0" key={i}>{tarea}</li>
+							<li
+								className="list-group-item border border-top-0"
+								key={i}>
+								{tarea.label}
+							</li>
 						);
 					})}
 				</ul>
